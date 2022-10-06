eace16347eb4423c032b1b9b55244ab460f51306
Mark Medum Bundgaard <mmedum@gmail.com>
2022-10-06 20:53:30 +0200

    Updating to output folders to delete

--- app/src/main/java/compare/dirs/Main.java
+++ app/src/main/java/compare/dirs/Main.java
@@ -8,17 +8,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
-    private static List<String> rootPaths;
-    private static Map<String, Double> directories;
-
     public static void main(String[] args) {
-        rootPaths = new ArrayList<>();
-        directories = new HashMap<>();
+        List<String> rootPaths = new ArrayList<>();
+        Map<String, Node> directories = new HashMap<>();
 
         for (String arg : args) {
             rootPaths.add(arg.strip());
@@ -29,14 +25,53 @@ public class Main {
         for (String rootPath : rootPaths) {
             try (Stream<Path> stream = Files.walk(Paths.get(rootPath))) {
                 System.out.println("Checking path: " + rootPath);
-                Map<String, Long> result = stream.map(f -> f.toFile())
+                stream.map(f -> f.toFile())
                         .filter(f -> f.isDirectory())
-                        .collect(Collectors.toMap(f -> f.getName(), f -> f.length(), (f1, f2) -> f2));
-
-                System.out.println(result);
+                        .forEach(f -> {
+                            String name = f.getName();
+                            String path = f.getPath();
+                            long size = f.length();
+                            if (directories.containsKey(name)) {
+                                Node tempNode = directories.get(name);
+                                if (size >= tempNode.getSize()) {
+                                    System.out
+                                            .println(tempNode.getName() + "@" + tempNode.getPath() + " to be deleted");
+                                    directories.put(name, new Node(name, path, size));
+                                } else {
+                                    System.out.println(name + "@" + path + " to be deleted");
+                                }
+                            } else {
+                                directories.put(f.getName(), new Node(f.getName(), f.getPath(), f.length()));
+                            }
+                        });
             } catch (IOException e) {
                 System.err.println("Cannot find path " + rootPath);
             }
+        }
+    }
+
+    private static class Node {
+
+        private String name;
+        private String path;
+        private long size;
+
+        public Node(String name, String path, long size) {
+            this.name = name;
+            this.path = path;
+            this.size = size;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public long getSize() {
+            return size;
         }
     }
 }
