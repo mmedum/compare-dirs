4461f6b226bcb32f6e5c261bf7f49e7489c8bde7
Mark Medum Bundgaard <mmedum@gmail.com>
2022-10-10 23:45:25 +0200

    Separating to different functions, to enable checking for other folder structures

--- app/src/main/java/compare/dirs/Main.java
+++ app/src/main/java/compare/dirs/Main.java
@@ -12,10 +12,15 @@ import java.util.stream.Stream;
 
 public class Main {
 
+    private static Map<String, Node> directories;
+    private static List<String> deletions;
+    private static List<String> rays;
+
     public static void main(String[] args) {
         List<String> rootPaths = new ArrayList<>();
-        Map<String, Node> directories = new HashMap<>();
-        List<String> deletions = new ArrayList<>();
+        directories = new HashMap<>();
+        deletions = new ArrayList<>();
+        rays = new ArrayList<>();
 
         for (String arg : args) {
             rootPaths.add(arg.strip());
@@ -31,35 +36,55 @@ public class Main {
                         .forEach(f -> {
                             String name = f.getName().toString();
                             String path = f.getPath().toString();
-                            long size = Long.MIN_VALUE;
-
-                            try (Stream<Path> subStream = Files.walk(Paths.get(path))) {
-                                size = subStream.filter(p -> Files.isRegularFile(p))
-                                        .filter(p -> p.getFileName().toString().endsWith(".mkv"))
-                                        .mapToLong(p -> p.toFile().length())
-                                        .sum();
-                            } catch (IOException e) {
-                                System.err.println("Not possible to find subpath");
-                            }
-
-                            if (size > 0 && directories.containsKey(name)) {
-                                Node tempNode = directories.get(name);
-                                if (size >= tempNode.getSize()) {
-                                    deletions.add(tempNode.getPath() + "@" + tempNode.getName());
-                                    directories.put(name, new Node(name, path, size));
-                                } else {
-                                    deletions.add(path + "@" + name);
-                                }
-                            } else {
-                                directories.put(name, new Node(name, path, size));
-                            }
+                            findDuplicates(name, path);
+                            findRays(name, path);
                         });
             } catch (IOException e) {
                 System.err.println("Cannot find path " + rootPath);
             }
         }
 
+        System.out.println("Deletions:");
         deletions.forEach(p -> System.out.println(p));
+
+        System.out.println("Rays:");
+        //rays.forEach(p -> System.out.println(p));
+
+    }
+
+    private static void findRays(String name, String path) {
+        try (Stream<Path> subStream = Files.walk(Paths.get(path))) {
+            subStream.filter(p -> Files.isDirectory(p))
+                    .filter(p -> p.getFileName().toString().equals("BDMV"))
+                    .forEach(p -> System.out.println(p));
+        } catch (IOException e) {
+            System.err.println("Not possible to find subpath");
+        }
+    }
+
+    private static void findDuplicates(String name, String path) {
+        long size = Long.MIN_VALUE;
+
+        try (Stream<Path> subStream = Files.walk(Paths.get(path))) {
+            size = subStream.filter(p -> Files.isRegularFile(p))
+                    .filter(p -> p.getFileName().toString().endsWith(".mkv"))
+                    .mapToLong(p -> p.toFile().length())
+                    .sum();
+        } catch (IOException e) {
+            System.err.println("Not possible to find subpath");
+        }
+
+        if (size > 0 && directories.containsKey(name)) {
+            Node tempNode = directories.get(name);
+            if (size >= tempNode.getSize()) {
+                deletions.add(tempNode.getPath() + "@" + tempNode.getName());
+                directories.put(name, new Node(name, path, size));
+            } else {
+                deletions.add(path + "@" + name);
+            }
+        } else {
+            directories.put(name, new Node(name, path, size));
+        }
     }
 
     private static class Node {
