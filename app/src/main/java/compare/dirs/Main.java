a3f214cdbd74d160adf2e523cdece048536dc4d8
Mark Medum Bundgaard <mmedum@gmail.com>
2022-10-11 20:04:40 +0200

    Stop walking into subfolders and just list files with filters

--- app/src/main/java/compare/dirs/Main.java
+++ app/src/main/java/compare/dirs/Main.java
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("Starting comparison");
 
         for (String rootPath : rootPaths) {
-            try (Stream<Path> stream = Files.walk(Paths.get(rootPath))) {
+            try (Stream<Path> stream = Files.list(Paths.get(rootPath))) {
                 System.out.println("Checking path: " + rootPath);
                 stream.map(f -> f.toFile())
                         .filter(f -> f.isDirectory())
@@ -48,15 +48,14 @@ public class Main {
         deletions.forEach(p -> System.out.println(p));
 
         System.out.println("Rays:");
-        //rays.forEach(p -> System.out.println(p));
-
+        rays.forEach(p -> System.out.println(p));
     }
 
     private static void findRays(String name, String path) {
-        try (Stream<Path> subStream = Files.walk(Paths.get(path))) {
+        try (Stream<Path> subStream = Files.list(Paths.get(path))) {
             subStream.filter(p -> Files.isDirectory(p))
                     .filter(p -> p.getFileName().toString().equals("BDMV"))
-                    .forEach(p -> System.out.println(p));
+                    .forEach(p -> rays.add(path));
         } catch (IOException e) {
             System.err.println("Not possible to find subpath");
         }
@@ -65,7 +64,7 @@ public class Main {
     private static void findDuplicates(String name, String path) {
         long size = Long.MIN_VALUE;
 
-        try (Stream<Path> subStream = Files.walk(Paths.get(path))) {
+        try (Stream<Path> subStream = Files.list(Paths.get(path))) {
             size = subStream.filter(p -> Files.isRegularFile(p))
                     .filter(p -> p.getFileName().toString().endsWith(".mkv"))
                     .mapToLong(p -> p.toFile().length())
@@ -77,30 +76,24 @@ public class Main {
         if (size > 0 && directories.containsKey(name)) {
             Node tempNode = directories.get(name);
             if (size >= tempNode.getSize()) {
-                deletions.add(tempNode.getPath() + "@" + tempNode.getName());
-                directories.put(name, new Node(name, path, size));
+                deletions.add(tempNode.getPath());
+                directories.put(name, new Node(path, size));
             } else {
-                deletions.add(path + "@" + name);
+                deletions.add(path);
             }
         } else {
-            directories.put(name, new Node(name, path, size));
+            directories.put(name, new Node(path, size));
         }
     }
 
     private static class Node {
 
-        private String name;
         private String path;
         private long size;
 
-        public Node(String name, String path, long size) {
-            this.name = name;
+        public Node(String path, long size) {
             this.path = path;
             this.size = size;
-        }
-
-        public String getName() {
-            return name;
         }
 
         public String getPath() {
